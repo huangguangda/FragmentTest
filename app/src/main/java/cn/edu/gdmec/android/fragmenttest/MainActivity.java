40e099707dc7e1ba61e8c17fc8bcbc8b5f829ef6
huangguangda <2397923107@qq.com>
2017-12-07 02:20:23 +0800

    ojbk

--- app/src/main/java/cn/edu/gdmec/android/fragmenttest/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/fragmenttest/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 FragmentManager fragmentManager = getFragmentManager ();
                 FragmentTransaction transaction = fragmentManager.beginTransaction ();
                 transaction.replace ( R.id.right_layout, fragment );
+                transaction.addToBackStack ( null );
                 transaction.commit ();
                 break;
             default:
